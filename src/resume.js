f47557988f9b11c59a8aeb0c7d96c116a23878ca
durbin3 <54672526+durbin3@users.noreply.github.com>
2022-08-12 14:43:25 -0400

    update Resume to 2022

--- src/resume.js
+++ src/resume.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import resumePDF from './static/files/resume.pdf';
+import resumePDF from './static/files/Resume_2022.pdf';
 import { Document, Page, pdfjs} from 'react-pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 class Resume extends Component {
