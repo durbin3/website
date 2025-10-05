c94ccbaf0d245c073d56dee31976943a849a3119
Eric Durbin <ericdurbinf@gmail.com>
2025-10-05 09:50:24 -0400

    run linter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,29 +4,29 @@ import { HomePage } from './pages/home-page/home-page';
 export const routes: Routes = [
     {
         path: '',
-        title: "Home",
+        title: 'Home',
         component: HomePage,
     },
     {
         path: 'projects',
-        title: "Projects",
+        title: 'Projects',
         loadComponent: () =>
             import('./pages/projects-page/projects-page').then((m) => m.ProjectsPage),
     },
     {
         path: 'challenges',
-        title: "Challenges",
+        title: 'Challenges',
         loadComponent: () =>
             import('./pages/challenges-page/challenges-page').then((m) => m.ChallengesPage),
     },
     {
         path: 'resume',
-        title: "Resume",
+        title: 'Resume',
         loadComponent: () => import('./pages/resume-page/resume-page').then((m) => m.ResumePage),
     },
     {
         path: 'work-experience',
-        title: "Work Experience",
+        title: 'Work Experience',
         loadComponent: () => import('./pages/work-page/work-page').then((m) => m.WorkPage),
-    }
+    },
 ];
--- src/app/nav/nav.ts
+++ src/app/nav/nav.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
 import { Route, Routes } from '@angular/router';
-import { RouterModule } from "@angular/router";
+import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-nav',
@@ -12,10 +12,9 @@ import { RouterModule } from "@angular/router";
 export class Nav implements OnInit {
     @Input() title: string = '';
     @Input() links!: Routes;
-    
+
     activeLink!: Route;
-    
-    
+
     ngOnInit(): void {
         if (this.links?.length) {
             this.activeLink = this.links[0];
