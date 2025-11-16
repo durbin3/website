5a6bfbf67f388c41c520a114248ed96ab293630e
durbin3 <54672526+durbin3@users.noreply.github.com>
2025-11-16 11:30:13 -0500

    wip srgan project component

---
+++ src/app/projects/srgan/srgan.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-srgan',
+  imports: [],
+  templateUrl: './srgan.html',
+  styleUrl: './srgan.scss'
+})
+export class Srgan {
+
+}
