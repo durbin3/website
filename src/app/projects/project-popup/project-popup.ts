72cf08587323adca3ac7ccd18afe6860dba69d90
durbin3 <54672526+durbin3@users.noreply.github.com>
2025-12-14 11:02:57 -0500

    create project popup slots

--- src/app/projects/project-popup/project-popup.ts
+++ src/app/projects/project-popup/project-popup.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from "@angular/material/dialog";
 
 @Component({
     selector: 'project-popup',
-    imports: [ MatDialogModule ],
+    imports: [ MatDialogModule, MatCardModule ],
     templateUrl: './project-popup.html',
     styleUrl: './project-popup.scss'
 })
