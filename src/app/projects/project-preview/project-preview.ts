51a0d783c718f16ec585f44a7726900a6b1b42b1
durbin3 <54672526+durbin3@users.noreply.github.com>
2025-12-19 16:37:24 -0500

    create different css for clickable elements

--- src/app/projects/project-preview/project-preview.ts
+++ src/app/projects/project-preview/project-preview.ts
@@ -2,9 +2,10 @@ import { Component, inject, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialog } from '@angular/material/dialog';
 import { Project } from '../project';
+import { NgClass } from "@angular/common";
 @Component({
     selector: 'project-preview',
-    imports: [MatCardModule],
+    imports: [MatCardModule, NgClass],
     templateUrl: './project-preview.html',
     styleUrl: './project-preview.scss',
 })
@@ -13,11 +14,14 @@ export class ProjectPreview {
     @Input() description: string = '';
     @Input() projectComponent?: typeof Project;
 
+    hover: boolean = false;
     readonly dialog = inject(MatDialog);
 
     openProject(): void {
         if (this.projectComponent) {
-            const dialogRef = this.dialog.open(this.projectComponent, {});
+            const dialogRef = this.dialog.open(this.projectComponent, {
+                autoFocus: false  
+            });
         }
     }
 
