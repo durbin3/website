5b39b113dc523eea9475c3dafa7eb28db74aeba0
durbin3 <54672526+durbin3@users.noreply.github.com>
2025-12-19 11:08:02 -0500

    migrate challenges page into projects page

--- src/app/pages/challenges-page/challenges-page.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-challenges-page',
-    imports: [],
-    templateUrl: './challenges-page.html',
-    styleUrl: './challenges-page.scss',
-})
-export class ChallengesPage {}
