d0b9c8df07e80a0e64c4a09ebf45f1df3997cabb
durbin3 <54672526+durbin3@users.noreply.github.com>
2025-12-19 10:07:50 -0500

    delete unused resume page component

--- src/app/pages/resume-page/resume-page.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-resume-page',
-    imports: [],
-    templateUrl: './resume-page.html',
-    styleUrl: './resume-page.scss',
-})
-export class ResumePage {}
