31910f1ce06d6c86834a107519895d05659f4845
durbin3 <54672526+durbin3@users.noreply.github.com>
2025-12-28 12:03:50 -0500

    change color theme and make nav colors more unified

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -11,14 +11,12 @@ export class Theme {
         document.body.className = this.getColorTheme();
     }
     setTheme(theme: ThemeOption) {
-        console.log('setting theme: ', theme)
         this.themeSignal.set(theme);
         document.body.className = this.getColorTheme();
     }
 
     getColorTheme() {
         const t = this.themeSignal();
-        console.log(t);
         return t + '-mode';
     }
 }
