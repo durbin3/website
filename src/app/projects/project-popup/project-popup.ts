0b8b3d76ae8113bb56f4511f67b01356f67e1ff9
durbin3 <54672526+durbin3@users.noreply.github.com>
2025-12-31 14:37:33 -0500

    adjust popup styles

--- src/app/projects/project-popup/project-popup.ts
+++ src/app/projects/project-popup/project-popup.ts
@@ -1,12 +1,17 @@
-import { Component, Input } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatDialogModule } from "@angular/material/dialog";
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule, MatDialogRef } from "@angular/material/dialog";
 
 @Component({
     selector: 'project-popup',
-    imports: [ MatDialogModule, MatCardModule ],
+    imports: [ MatDialogModule, MatButtonModule ],
     templateUrl: './project-popup.html',
     styleUrl: './project-popup.scss'
 })
 export class ProjectPopup {
+    private readonly dialogRef = inject(MatDialogRef);
+
+    onClose() {
+        this.dialogRef.close();
+    }
 }
