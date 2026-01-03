7e46a30996039d18130faae41cdee14473ebb1e2
durbin3 <54672526+durbin3@users.noreply.github.com>
2026-01-03 11:47:28 -0500

    create wip message

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Nav } from './nav/nav';
 import { pages } from './app.routes';
 import { Theme } from './theme';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, Nav],
+    imports: [RouterOutlet, Nav, MatButtonModule],
     templateUrl: './app.html',
     styleUrl: './app.scss',
 })
@@ -14,4 +15,5 @@ export class App {
     readonly theme = inject(Theme);
     protected readonly title = signal('Eric Durbin - Software Engineer');
     pages = pages;
+    showWIP: boolean = true;
 }
