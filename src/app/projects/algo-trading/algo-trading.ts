d9ddb6ea6cbc9e845215bfa5dd7b9986e2fc7636
durbin3 <54672526+durbin3@users.noreply.github.com>
2026-01-10 10:35:59 -0500

    create algo_trading project stub files

---
+++ src/app/projects/algo-trading/algo-trading.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'algo-trading',
+  imports: [],
+  templateUrl: './algo-trading.html',
+  styleUrl: './algo-trading.scss'
+})
+export class AlgoTrading {
+
+}
